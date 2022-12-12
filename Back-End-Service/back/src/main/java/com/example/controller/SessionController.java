65c5c1f8531ded0f16e83f40d63b7979ddc853bd
Pick-cmd <728820587@qq.com>
2022-12-12 15:05:14 +0800

    完善后端代码

--- Back-End-Service/back/src/main/java/com/example/controller/SessionController.java
+++ Back-End-Service/back/src/main/java/com/example/controller/SessionController.java
@@ -24,13 +24,16 @@ public class SessionController {
     //具体展示某一条数据
     @GetMapping("/getArticle")
     public String selectSingle(HttpServletRequest request) {
+        //创建一个session对象，获取id的值
         HttpSession session = request.getSession();
         long id = (long) session.getAttribute("id");
 
+        //根据id的值查询对应的文章
         QueryWrapper<Essays> queryWrapper = new QueryWrapper<>();  
         queryWrapper.eq("id", id);
         Essays article = essayMapper.selectOne(queryWrapper);
 
+        //返回文章的相应数据
         LinkedHashMap<String, Object> all_data = new LinkedHashMap<>();
         all_data.put("article", article);
         all_data.put("code", 200);
---
+++ Back-End-Service/back/src/main/java/com/example/mapper/TopicMapper.java
@@ -0,0 +1,9 @@
+package com.example.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.example.pojo.Topic;
+
+public interface TopicMapper extends BaseMapper<Topic>{
+
+    Topic selectAll = null;
+}
